fcf4490683e4b67a9b7016a728b7404c6cd588f6
chaptercool <yr.hvrn15@gmail.com>
2024-07-05 18:27:22 +0200

    Timer implementation started

--- app/src/main/java/com/active/teethtime/TimerActivity.kt
+++ app/src/main/java/com/active/teethtime/TimerActivity.kt
@@ -1,7 +1,12 @@
 package com.active.teethtime
 
+import android.annotation.SuppressLint
+import android.content.Context.VIBRATOR_SERVICE
+import android.content.Intent
 import androidx.compose.foundation.Image
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,24 +21,41 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.active.teethtime.ui.theme.AppTheme
+import kotlinx.coroutines.delay
 
 class TimerActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,84 +63,144 @@ class TimerActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                Timer()
+                ToothbrushTimerScreen(
+//                    onTimerFinished = {
+//                        // Replace with the action to take when the timer finishes
+//                        val intent = Intent(this, MainActivity::class.java)
+//                        startActivity(intent)
+//                        finish()
+//                    }
+                )
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Timer(modifier: Modifier = Modifier){
-    Surface (color = MaterialTheme.colorScheme.primaryContainer){
-        Column(
-            modifier = modifier,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            val ExampleProgress = 50
-            CircularProgressIndicator(progress = { ExampleProgress.toFloat() }, modifier.size(230.dp))
+fun ToothbrushTimerScreen(modifier: Modifier = Modifier) {
+    var series by remember { mutableStateOf(0) }
+    var timeLeft by remember { mutableStateOf(20) }
+    var timerRunning by remember { mutableStateOf(true) }
+    var showDialog by remember { mutableStateOf(false) }
 
+    val seriesImages = listOf(
+        R.drawable.test,
+        R.drawable.test2,
+        R.drawable.test3,
+        R.drawable.test4,
+        R.drawable.test5,
+        R.drawable.test6
+    )
+    val context = LocalContext.current
+    LaunchedEffect(series, timerRunning) {
+
+        val vibrator = context.getSystemService(VIBRATOR_SERVICE) as Vibrator
+        if (timerRunning && series < 6) {
+            for (second in 20 downTo 1) {
+                delay(1000L)
+                timeLeft = second
+            }
+            vibrator.vibrate(
+                VibrationEffect.createOneShot(
+                    500L,
+                    VibrationEffect.DEFAULT_AMPLITUDE
+                )
+            )
+            series++
+            timeLeft = 20
+            if (series == 6) {
+//                onTimerFinished()
+            }
         }
-        ImageDisplay()
-        TimeRow()
-        Row(
-            modifier = Modifier
-                .padding(30.dp)
+    }
+
+        Surface(color = MaterialTheme.colorScheme.primaryContainer) {
+            ProgrCircle(tl = timeLeft)
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 120.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+
+                Image(
+                    painter = painterResource(id = seriesImages[series.coerceAtMost(5)]),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(150.dp)
+                )
+
+                Spacer(modifier = Modifier.height(55.dp))
+
+                Text(
+                    text = "$timeLeft",
+                    fontSize = 48.sp,
+                )
+            }
+            Row(modifier = Modifier
+                .padding(35.dp)
                 .fillMaxSize()
                 .fillMaxWidth()
                 .fillMaxHeight(),
-            verticalAlignment = Alignment.Top,
-            horizontalArrangement = Arrangement.Absolute.Left
-        ){
-            ExtendedFloatingActionButton(
-                onClick = { /* todo*/ },
-                icon = { Icon(painter = painterResource(R.drawable.back), contentDescription = "Back") },
-                text = { Text(text = "Cancel") },
-            )
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.Absolute.Left)
+            {
+                FloatingActionButton(onClick = { showDialog = true } ) {
+                    Icon(painter = painterResource(R.drawable.back), contentDescription = "Cancel")
+                }
+            }
+        }
 
+
+        if (showDialog) {
+            val context = LocalContext.current
+            val intent = Intent(context, MainActivity::class.java)
+            AlertDialog(
+                onDismissRequest = { showDialog = false },
+                title = { Text("Cancel Timer") },
+                text = { Text("Are you sure you want to cancel the timer?") },
+                confirmButton = {
+                    TextButton(onClick = { context.startActivity(intent) }) {
+                        Text("Yes")
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = { showDialog = false }) {
+                        Text("No")
+                    }
+                }
+            )
         }
     }
 
-
-}
-
 @Composable
-fun TimeRow(modifier: Modifier = Modifier){
-    Row(
-        modifier = Modifier
-            .padding(top = 300.dp)
+fun ProgrCircle(tl: Int, modifier: Modifier = Modifier){
+    Column (
+        modifier = modifier
             .fillMaxSize()
             .fillMaxWidth()
             .fillMaxHeight(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ){
-
-        val Seconds = Text(text = "16", fontSize = 30.sp)
-        Text(text = " s", fontSize = 18.sp, color = Color.Gray)
-    }
-}
-
-@Composable
-fun ImageDisplay(modifier: Modifier = Modifier){
-    Column (
-        modifier = modifier
-            .fillMaxHeight()
-            .fillMaxWidth()
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-       val img = Image(
-            painterResource(R.drawable.test), contentDescription = "Test", Modifier.size(120.dp)
-       )
+        CircularProgressIndicator(
+            progress = (20 - tl) / 20f ,
+            modifier = Modifier
+                .size(300.dp)
+                .padding(16.dp)
+                .clip(CircleShape),
+            strokeWidth = 5.dp
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun TimerPreview() {
-    Timer(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
-    )
+    ToothbrushTimerScreen(modifier = Modifier)
 }
+
+// onTimerFinished: () -> Unit
